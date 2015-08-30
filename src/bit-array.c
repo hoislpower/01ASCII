0ff087f5a623b01a85a74205288611bc614d8041
Patrick Hoislbauer <patrick.hoislbauer@gmail.com>
2015-08-30 08:43:55 +0200

    Added a dynamic datastructure for the bit array.

---
+++ src/bit-array.c
@@ -0,0 +1,190 @@
+/*
+    01ASCII
+
+    Converter for hexadecimal or binary files into textfiles containing 
+    the data in form of ASCII 0s and 1s.
+
+    Authored By Patrick Hoislbauer  <patrick.hoislbauer@gmail.com>
+
+    Copyright (C) 2015 Patrick Hoislbauer
+
+    This file is part of 01ASCII.
+
+    01ASCII is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    01ASCII is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 01ASCII.  If not, see <http://www.gnu.org/licenses/>.
+
+    Diese Datei ist Teil von 01ASCII.
+
+    01ASCII ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    01ASCII wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#include "bit-array.h"
+
+
+// global Variables
+static	int8_t *array = NULL;
+static	int arrayIndex;
+static	int arrayLength;
+
+
+// Initializes the bit array
+// This procedure must be called once before using the other procedures.
+// -----------------------------------------------------------------------------
+// IN length: Length of the new bit array.
+// RETURNS: EXIT_FAILURE if a failure occurred, EXIT_SUCCESS otherwise.
+// -----------------------------------------------------------------------------
+int	initializeBitArray(int length)
+{
+	int i;
+
+
+	arrayLength = length;
+	arrayIndex = 0;
+
+	// allocate memory for the array and an additional buffer for 
+	// temporarily storing parts of the bit array
+	array = malloc(arrayLength*sizeof(int8_t));
+	if(array == NULL)
+	{
+		fprintf(stderr, "ERROR: Could not allocate memory for bit "
+			"array.\r\n");
+		return EXIT_FAILURE;
+	}
+
+	// initialize the array with unused bits
+	for(i=0; i<arrayLength; i++)
+		array[i] = UNUSED_BIT;
+
+	return EXIT_SUCCESS;
+}
+
+
+// Disposes the bit array
+// This procedure must be called once when the bit array is no longer used.
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void	disposeBitArray()
+{
+	if(array != NULL)
+		free(array);
+}
+
+
+// Get the bit array
+// The returned bit array must not be written.
+// -----------------------------------------------------------------------------
+// RETURNS: The bit array.
+// -----------------------------------------------------------------------------
+int8_t*	getBitArray()
+{
+	return array;
+}
+
+
+// Get the current index.
+// -----------------------------------------------------------------------------
+// RETURNS: Current index within the bit array.
+// -----------------------------------------------------------------------------
+int	bitArrayGetCurrentIndex()
+{
+	return arrayIndex;
+}
+
+
+// Appends a bit range to the bit array
+// e.g.: When startBit=3 and endBit=5 then 3,4,5 is added to the array.
+// -----------------------------------------------------------------------------
+// IN startBit: Start bit of the range.
+// IN endBit: End bit of the range.
+// RETURNS: EXIT_FAILURE if the result exceeded the length of the array,
+//          EXIT_SUCCESS otherwise.
+//          If EXIT_FAILURE is returned the bit array will be left unchanged.
+// -----------------------------------------------------------------------------
+int	bitArrayAdd(int8_t startBit, int8_t endBit)
+{
+	int i;
+
+
+	// check if the length of the array will be exceeded
+	if(arrayIndex + endBit-startBit+1 > arrayLength)
+		return EXIT_FAILURE;
+
+	// add the bits to the array
+	for(i=startBit; i<=endBit; i++)
+	{
+		array[arrayIndex] = i;
+		arrayIndex++;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+
+// Repeat parts of the array.
+// -----------------------------------------------------------------------------
+// IN startIndex: Start index of the array part to repeat.
+//                (The repeat includes the element at this index.)
+// IN endIndex: End index of the array part to repeat.
+//              (The repeat includes the element at this index.)
+// IN repeatNum: Number of repeats.
+// RETURNS: EXIT_FAILURE if the result exceeded the length of the array,
+//          EXIT_SUCCESS otherwise.
+//          If EXIT_FAILURE is returned the bit array will be left unchanged.
+// -----------------------------------------------------------------------------
+int	bitArrayRepeat(int startIndex, int endIndex, int repeatNum)
+{
+	int i;
+	int sequenceLength;
+
+
+	// calculate the length of the sequence
+	sequenceLength = endIndex - startIndex + 1;
+
+	// exit if there is nothing to repeat (return no failure)
+	if(sequenceLength < 1)
+		return EXIT_SUCCESS;
+
+	// check if the length of the array will be exceeded
+	if(arrayIndex + sequenceLength*repeatNum > arrayLength)
+		return EXIT_FAILURE;
+
+	// shift the part which follows the repeat section
+	// to the end of the array
+	memcpy(array+endIndex+1+sequenceLength*repeatNum, array+endIndex+1, 
+		arrayIndex-endIndex-1);
+
+	// repeat the given bit sequence
+	for(i=0; i<repeatNum; i++)
+	{
+		memcpy(array+endIndex+1+i*sequenceLength, array+startIndex,
+			sequenceLength);
+		arrayIndex += sequenceLength;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+
---
+++ tests/bitarray_tests.c
@@ -0,0 +1,138 @@
+#include <config.h>
+#include <check.h>
+
+#include "../src/bit-array.h"
+
+
+#define	ARRAY_SIZE	12
+
+
+// test all bit array procedures and functions
+START_TEST(bitArrayTest)
+{
+	int i;
+	int8_t *array;
+
+
+	// initialize the bit array with size ARRAY_SIZE
+	ck_assert_int_eq(initializeBitArray(ARRAY_SIZE), EXIT_SUCCESS);
+
+	// check if a valid array will be returned
+	array = NULL;
+	array = getBitArray();
+	ck_assert_int_ne(array, NULL);
+
+	// check the current index
+	ck_assert_int_eq(bitArrayGetCurrentIndex(), 0);
+
+	// check if the array has been initialized
+	for(i=0; i<ARRAY_SIZE; i++)
+		ck_assert_int_eq(array[i], UNUSED_BIT);
+
+	// add values to the bit array
+	ck_assert_int_eq(bitArrayAdd(LITERAL1_BIT, LITERAL1_BIT), EXIT_SUCCESS);
+	ck_assert_int_eq(bitArrayAdd(5, 5), EXIT_SUCCESS);
+	ck_assert_int_eq(bitArrayAdd(LITERAL0_BIT, LITERAL0_BIT), EXIT_SUCCESS);
+	ck_assert_int_eq(bitArrayAdd(3, 6), EXIT_SUCCESS);
+
+	// try an invalid range
+	ck_assert_int_eq(bitArrayAdd(6, 3), EXIT_SUCCESS);
+
+	// try to add too much values to the array
+	ck_assert_int_eq(bitArrayAdd(1, 6), EXIT_FAILURE);
+
+	// check the current index
+	ck_assert_int_eq(bitArrayGetCurrentIndex(), 7);
+
+	// check the content of the array
+	ck_assert_int_eq(array[0], LITERAL1_BIT);
+	ck_assert_int_eq(array[1], 5);
+	ck_assert_int_eq(array[2], LITERAL0_BIT);
+	ck_assert_int_eq(array[3], 3);
+	ck_assert_int_eq(array[4], 4);
+	ck_assert_int_eq(array[5], 5);
+	ck_assert_int_eq(array[6], 6);
+	ck_assert_int_eq(array[7], UNUSED_BIT);
+	ck_assert_int_eq(array[8], UNUSED_BIT);
+	ck_assert_int_eq(array[9], UNUSED_BIT);
+	ck_assert_int_eq(array[10], UNUSED_BIT);
+	ck_assert_int_eq(array[11], UNUSED_BIT);
+
+
+	// try to repeat a part of the array
+	ck_assert_int_eq(bitArrayRepeat(3, 4, 3), EXIT_FAILURE);
+
+	// repeat a part of the array
+	ck_assert_int_eq(bitArrayRepeat(3, 4, 2), EXIT_SUCCESS);
+
+	// try an invalid repeat
+	ck_assert_int_eq(bitArrayRepeat(5, 3, 1), EXIT_SUCCESS);
+
+	// check the current index
+	ck_assert_int_eq(bitArrayGetCurrentIndex(), 11);
+
+	// check the content of the array
+	ck_assert_int_eq(array[0], LITERAL1_BIT);
+	ck_assert_int_eq(array[1], 5);
+	ck_assert_int_eq(array[2], LITERAL0_BIT);
+	ck_assert_int_eq(array[3], 3);
+	ck_assert_int_eq(array[4], 4);
+	ck_assert_int_eq(array[5], 3);
+	ck_assert_int_eq(array[6], 4);
+	ck_assert_int_eq(array[7], 3);
+	ck_assert_int_eq(array[8], 4);
+	ck_assert_int_eq(array[9], 5);
+	ck_assert_int_eq(array[10], 6);
+	ck_assert_int_eq(array[11], UNUSED_BIT);
+
+	// add one more value to the array
+	ck_assert_int_eq(bitArrayAdd(9, 9), EXIT_SUCCESS);
+
+	// check the current index
+	ck_assert_int_eq(bitArrayGetCurrentIndex(), 12);
+
+	// try to add one more value to the array
+	ck_assert_int_eq(bitArrayAdd(8, 8), EXIT_FAILURE);
+
+	// dispose the array
+	disposeBitArray();
+}
+END_TEST
+
+
+Suite	*testSuite()
+{
+	Suite *suite;
+	TCase *testCase;
+
+
+	suite = suite_create("Bit Array");
+
+
+	// test cases for bit array procedures an functions
+	testCase = tcase_create("bitArray");
+	tcase_add_test(testCase, bitArrayTest);
+	suite_add_tcase(suite, testCase);
+
+	return suite;
+}
+
+
+int main(void)
+{
+	int testResult;
+	Suite *suite;
+	SRunner *suiteRunner;
+
+
+	// create the test suite and the runner
+	suite = testSuite();
+	suiteRunner = srunner_create(suite);
+
+	// execute the tests
+	srunner_run_all(suiteRunner, CK_NORMAL);
+	testResult = srunner_ntests_failed(suiteRunner);
+	srunner_free(suiteRunner);
+
+	return (testResult == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
